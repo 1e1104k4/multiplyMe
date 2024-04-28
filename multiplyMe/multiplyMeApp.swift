0e868041b4768b79174833b2492e7fd7fe9de983
leila <143362178+1e1104k4@users.noreply.github.com>
2024-04-28 17:11:00 -0500

    Initial Commit

---
+++ multiplyMe/multiplyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplyMeApp.swift
+//  multiplyMe
+//
+//  Created by Leila on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct multiplyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
